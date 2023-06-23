5defc00a99e7df26053a0e9f35f13a15729c8a38
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-06-23 16:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const button=document.getElementById("remove-btn");
+
+const colourSelector=document.getElementById("colorSelect");
+
+button.addEventListener("click",removeElement);
+
+function removeElement(){
+	const val=colourSelector.value;
+
+	if(colourSelector.children){
+		for(let i=0;i<colourSelector.children.length;i++){
+			if(colourSelector.children[i].innerHTML===val){
+				colourSelector.removeChild(colourSelector.children[i]);
+			}
+		}
+	}
+	
+}
